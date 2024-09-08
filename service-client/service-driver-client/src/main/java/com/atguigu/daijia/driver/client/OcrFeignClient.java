efdea355438a9925d6d7e589375b162f0f6c5015
WangXing <19846811030@163.com>
2024-09-08 22:14:06 +0800

    feat : 身份证、驾驶证校验

--- service-client/service-driver-client/src/main/java/com/atguigu/daijia/driver/client/OcrFeignClient.java
+++ service-client/service-driver-client/src/main/java/com/atguigu/daijia/driver/client/OcrFeignClient.java
@@ -12,5 +12,20 @@ import org.springframework.web.multipart.MultipartFile;
 @FeignClient(value = "service-driver")
 public interface OcrFeignClient {
 
+    /**
+     * 身份证识别
+     * @param file
+     * @return
+     */
+    @PostMapping(value = "/ocr/idCardOcr", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    Result<IdCardOcrVo> idCardOcr(@RequestPart("file") MultipartFile file);
+
+    /**
+     * 驾驶证识别
+     * @param file
+     * @return
+     */
+    @PostMapping(value = "/ocr/driverLicenseOcr", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    Result<DriverLicenseOcrVo> driverLicenseOcr(@RequestPart("file") MultipartFile file);
 
 }
